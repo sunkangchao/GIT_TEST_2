8a639b26bc84681c61b920d27291d279bac8c825
sunkangchao <928893296@qq.com>
2020-08-04 01:03:49 +0800

    第九次提交git   --2020.8.4

--- .idea/java/controller/Test2.java
+++ .idea/java/controller/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("hello-world2");
         System.out.println("测试回滚");
+        System.out.println("测试使用命令提交到github");
     }
 }
