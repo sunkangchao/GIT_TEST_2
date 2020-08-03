d1097a96defefd6b54aed469ee45345d30a27a9c
sunkangchao <928893296@qq.com>
2020-08-04 01:12:11 +0800

    Merge branch 'slave1'
    # Conflicts:
    #	.idea/java/controller/Test2.java

--- .idea/java/controller/Test2.java
+++ .idea/java/controller/Test2.java
@@ -2,7 +2,6 @@ package controller;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("hello-world2");
-        System.out.println("测试分支");
-    }
-}
+
+        }
+        }
