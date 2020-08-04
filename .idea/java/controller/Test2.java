f1f19b08c8206fe190615ccfe455f765ef41748d
sunkangchao <928893296@qq.com>
2020-08-04 21:21:49 +0800

    第十二次提交

--- .idea/java/controller/Test2.java
+++ .idea/java/controller/Test2.java
@@ -3,8 +3,13 @@ package controller;
 public class Test2 {
     public static void main(String[] args) {
 
-    }
+        System.out.println("合并分支");
+        System.out.println("拉取代码成功");
+        System.out.println("什么情况下需要解决冲突");
+        System.out.println("被合并的分支上有被修改的地方需要解决冲突");
+        System.out.println("什么情况下不需要解决冲突");
+        System.out.println("被合并的分支上的原代码没有被修改过，只是增加了");
+        }
 }
 
-
-
+//dfjdsklf
