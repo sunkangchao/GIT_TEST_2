19faa007ee8595a117416835993f793d750820e3
sunkangchao <928893296@qq.com>
2020-08-29 17:06:48 +0800

    salve2分支新增了Test3和Test4类。

---
+++ .idea/java/controller/Test3.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+        System.out.println("22222222222");
+    }
+}
---
+++ .idea/java/controller/Test4.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class Test4 {
+    public static void main(String[] args) {
+        System.out.println("33333333333---salve2分支");
+    }
+}
